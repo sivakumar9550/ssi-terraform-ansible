c5282a62de6fff0dcc1d0137a053a6ed563571c3
sivakumar9550 <103304315+sivakumar9550@users.noreply.github.com>
2023-05-01 01:03:21 +0530

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 data "aws_route53_zone" "hosted_zone" {
-  name = var.mail_domain_name
+  name = var.my_domain_name
 }
 
 locals {
@@ -13,7 +13,7 @@ locals {
 
 resource "aws_route53_record" "mail" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = var.mail_sub_domain_name
+  name    = var.my_mail_sub_domain_name
   type    = "A"
   ttl     = 300
   records = [aws_instance.mail_server[0].public_ip]
@@ -21,7 +21,7 @@ resource "aws_route53_record" "mail" {
 
 resource "aws_route53_record" "db" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = var.db_sub_domain_name
+  name    = var.my_db_sub_domain_name
   type    = "A"
   ttl     = 300
   records = [aws_instance.db_server[0].public_ip]
@@ -29,7 +29,7 @@ resource "aws_route53_record" "db" {
 
 resource "aws_route53_record" "httpd_server" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = var.httpd_server_sub_domain_name
+  name    = var.my_httpd_server_sub_domain_name
   type    = "A"
   ttl     = 300
   records = [aws_instance.httpd_server[0].public_ip]
@@ -37,7 +37,7 @@ resource "aws_route53_record" "httpd_server" {
 
 resource "aws_route53_record" "web" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = var.web_sub_domain_name
+  name    = var.my_web_sub_domain_name
   type    = "A"
   ttl     = 300
   records = [aws_instance.web_server[0].public_ip]

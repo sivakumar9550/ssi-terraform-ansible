b70a612d4ed2b3e234847b53d1cc48fcb7e82dbf
sivakumar9550 <103304315+sivakumar9550@users.noreply.github.com>
2023-05-01 00:51:09 +0530

    Update web.tf

--- web.tf
+++ web.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web_server" {
   count = var.web_want == true ? var.web_count : 0
   ami           = "ami-02eb7a4783e7e9317"
   instance_type = var.instance_type
-  key_name      = var.key_name
+  key_name      = var.my_key_name
   
   connection {
     type     = "ssh"

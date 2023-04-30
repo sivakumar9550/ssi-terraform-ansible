d2c327fc897ef823d92141c1d376e7849048561b
sivakumar9550 <103304315+sivakumar9550@users.noreply.github.com>
2023-05-01 00:50:39 +0530

    Update mail.tf

--- mail.tf
+++ mail.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mail_server" {
   count = var.mail_want == true ? var.mail_count : 0 
   ami           = "ami-02eb7a4783e7e9317"
   instance_type = var.instance_type
-  key_name      = var.key_name
+  key_name      = var.my_key_name
   
   connection {
     type     = "ssh"

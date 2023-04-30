4dcc11f19aa4241dc429ebc0a8078280902550ac
sivakumar9550 <103304315+sivakumar9550@users.noreply.github.com>
2023-05-01 01:19:31 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,16 @@
+
+output "web_server_public_ip" {
+  value = aws_instance.web_server.*.public_ip
+}
+
+output "mail_server_public_ip" {
+  value = aws_instance.mail_server.*.public_ip
+}
+
+output "db_server_public_ip" {
+  value = aws_instance.db_server.*.public_ip
+}
+
 output "httpd_server_public_ip" {
   value = aws_instance.httpd_server.*.public_ip
 }

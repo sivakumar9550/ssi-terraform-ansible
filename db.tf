db980744bf7de9209d5b9d799a0c25251a327b3e
sivakumar9550 <103304315+sivakumar9550@users.noreply.github.com>
2023-05-01 00:49:46 +0530

    Update db.tf

--- db.tf
+++ db.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "db_server" {
   count = var.db_want == true ? var.db_count : 0
   ami           = "ami-02eb7a4783e7e9317"
   instance_type = var.instance_type
-  key_name      = var.key_name
+  key_name      = var.my_key_name
   
   connection {
     type     = "ssh"
